18a39a6dd596bbf9d198410bafcec39289cd7332
stefaniacerboni <cerbonistefania@gmail.com>
2020-11-25 11:18:09 +0100

    error

--- src/main/java/it/unifi/stlab/DTO/faultTree/NodeType.java
+++ src/main/java/it/unifi/stlab/DTO/faultTree/NodeType.java
@@ -3,10 +3,10 @@ package it.unifi.stlab.DTO.faultTree;
 public enum NodeType {
 
 	BASIC_EVENT,
-	GATE,
-	FAILURE;
-
-	public static NodeType fromString(String className ) {
+	FAILURE,
+	GATE;
+	
+	public static NodeType fromString( String className ) {
 		switch (className) {
 		case "BasicEvent":
 			return BASIC_EVENT;
