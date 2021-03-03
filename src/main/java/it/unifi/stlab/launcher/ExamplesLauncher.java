5beaa95004a4ddf21c28fb334549b1b39fe92f96
stefaniacerboni <cerbonistefania@gmail.com>
2021-03-03 19:46:56 +0100

    -Aggiunti builder dei tre sistemi PollutionMonitor: Preliminary_Design, Target_Design e Ideal_Design

--- src/main/java/it/unifi/stlab/launcher/ExamplesLauncher.java
+++ src/main/java/it/unifi/stlab/launcher/ExamplesLauncher.java
@@ -3,29 +3,19 @@ package it.unifi.stlab.launcher;
 import it.unifi.stlab.faultflow.exporter.PetriNetExportMethod;
 import it.unifi.stlab.faultflow.exporter.PetriNetExporter;
 import it.unifi.stlab.faultflow.model.knowledge.composition.System;
-import it.unifi.stlab.faultflow.model.knowledge.propagation.FailureMode;
-import it.unifi.stlab.faultflow.model.knowledge.propagation.FaultMode;
-import it.unifi.stlab.faultflow.model.operational.Failure;
-import it.unifi.stlab.faultflow.model.operational.Fault;
 import it.unifi.stlab.faultflow.model.operational.Scenario;
 import it.unifi.stlab.faultflow.translator.PetriNetTranslator;
-import it.unifi.stlab.faultflow.translator.PetriNetTranslatorMethod;
-import it.unifi.stlab.launcher.systembuilder.PollutionMonitorIdealDesignBuilder;
-import it.unifi.stlab.launcher.systembuilder.PollutionMonitorPreliminaryDesignBuiler;
-import it.unifi.stlab.launcher.systembuilder.SimpleSystem02Builder;
-import it.unifi.stlab.launcher.systembuilder.SteamBoilerModelBuilder;
+import it.unifi.stlab.launcher.systembuilder.PollutionMonitorModelBuilder;
 
 import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
-import java.math.BigDecimal;
-import java.util.HashMap;
 
 public class ExamplesLauncher {
 
     public static void main(String[] args) throws JAXBException, FileNotFoundException {
         //Per cambiare sistema da buildare è sufficiente modificare l'istruzione seguente
         //con il builder del sistema desiderato.
-        System s = PollutionMonitorIdealDesignBuilder.getInstance().getSystem();
+        System s = PollutionMonitorModelBuilder.getInstance().getSystem();
         //Exporting petri net as fault injection -faults with deterministic occurrence sampled from the pdfs
         PetriNetExporter.exportPetriNetFromSystem(s, PetriNetExportMethod.FAULT_INJECTION);
         //Exporting petri net as fault analysis -faults with their original pdf
@@ -33,7 +23,7 @@ public class ExamplesLauncher {
 
         Scenario scenario = new Scenario(s, "_Serial");
         scenario.InitializeScenarioFromSystem();
-        scenario.accept(pnt, PetriNetTranslatorMethod.CONCURRENT);
+        scenario.accept(pnt);
         PetriNetExporter.exportPetriNetOrderedByComponent(pnt, s);
         scenario.propagate();
         scenario.printReport();
