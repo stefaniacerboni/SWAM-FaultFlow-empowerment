dfe17cabd3251f61a43011a1e9954d7361e4cb62
stefaniacerboni <cerbonistefania@gmail.com>
2021-03-05 19:43:51 +0100

    -Aggiornati BUILDERS dei tre sistemi PollutionMonitor: Preliminary_Design, Target_Design e Ideal_Design

--- src/main/java/it/unifi/stlab/launcher/systembuilder/PollutionMonitorIdealDesignBuilder.java
+++ src/main/java/it/unifi/stlab/launcher/systembuilder/PollutionMonitorIdealDesignBuilder.java
@@ -27,18 +27,13 @@ public class PollutionMonitorIdealDesignBuilder{
         Component persistenceSystem = new Component("PersistenceSystem");
         Component smartAgent = new Component("SmartAgent");
         Component dataStorage = new Component("DataStorage");
-        Component ingestionSubsystemOzone = new Component("IngestionSubsystemOzone");
         Component ingestionSubsystem1PM10 = new Component("IngestionSubsystem1PM10");
         Component ingestionSubsystem2PM10 = new Component("IngestionSubsystem2PM10");
         Component ingestionSubsystem3PM10 = new Component("IngestionSubsystem3PM10");
-        Component iotBroker = new Component("IoTBroker");
-        Component fieldDeviceSubsystemOzone = new Component("FieldDeviceSubsystemOzone");
         Component fieldDeviceSubsystem1PM10 = new Component("FieldDeviceSubsystem1PM10");
         Component fieldDeviceSubsystem2PM10 = new Component("FieldDeviceSubsystem2PM10");
         Component fieldDeviceSubsystem3PM10 = new Component("FieldDeviceSubsystem3PM10");
-        Component mapReduceProcessorOzone = new Component("MapReduceProcessorOzone");
         Component mapReduceProcessorPM10 = new Component("MapReduceProcessorPM10");
-        Component ozoneSensor = new Component("OzoneSensor");
         Component pm10Sensor1 = new Component("PM10Sensor1");
         Component pm10Sensor2 = new Component("PM10Sensor2");
         Component pm10Sensor3 = new Component("PM10Sensor3");
@@ -47,10 +42,9 @@ public class PollutionMonitorIdealDesignBuilder{
         system.addComponent(pollutionMonitor,
                 persistenceSystem, smartAgent,
                 dataStorage, ingestionSubsystem1PM10, ingestionSubsystem2PM10, ingestionSubsystem3PM10,
-                ingestionSubsystemOzone, iotBroker,
-                fieldDeviceSubsystemOzone, fieldDeviceSubsystem1PM10, fieldDeviceSubsystem2PM10, fieldDeviceSubsystem3PM10,
-                mapReduceProcessorOzone, mapReduceProcessorPM10,
-                ozoneSensor, pm10Sensor1, pm10Sensor2, pm10Sensor3);
+                fieldDeviceSubsystem1PM10, fieldDeviceSubsystem2PM10, fieldDeviceSubsystem3PM10,
+                mapReduceProcessorPM10,
+                pm10Sensor1, pm10Sensor2, pm10Sensor3);
         system.setTopLevelComponent(pollutionMonitor);
 
         CompositionPort pm10Fds3 = new CompositionPort(pm10Sensor3, fieldDeviceSubsystem3PM10);
@@ -60,17 +54,12 @@ public class PollutionMonitorIdealDesignBuilder{
         CompositionPort pm10Fds1 = new CompositionPort(pm10Sensor1, fieldDeviceSubsystem1PM10);
         fieldDeviceSubsystem1PM10.addCompositionPorts(pm10Fds1);
 
-        CompositionPort osFdsO = new CompositionPort(ozoneSensor, fieldDeviceSubsystemOzone);
-        fieldDeviceSubsystemOzone.addCompositionPorts(osFdsO);
-
         CompositionPort fds1Is = new CompositionPort(fieldDeviceSubsystem1PM10, ingestionSubsystem1PM10);
         ingestionSubsystem1PM10.addCompositionPorts(fds1Is);
         CompositionPort fds2Is = new CompositionPort(fieldDeviceSubsystem2PM10, ingestionSubsystem2PM10);
         ingestionSubsystem2PM10.addCompositionPorts(fds2Is);
         CompositionPort fds3Is = new CompositionPort(fieldDeviceSubsystem3PM10, ingestionSubsystem3PM10);
         ingestionSubsystem3PM10.addCompositionPorts(fds3Is);
-        CompositionPort fdsoIs = new CompositionPort(fieldDeviceSubsystemOzone, ingestionSubsystemOzone);
-        ingestionSubsystemOzone.addCompositionPorts(fdsoIs);
 
         CompositionPort mrPM10Is1 = new CompositionPort(mapReduceProcessorPM10, ingestionSubsystem1PM10);
         ingestionSubsystem1PM10.addCompositionPorts(mrPM10Is1);
@@ -78,17 +67,11 @@ public class PollutionMonitorIdealDesignBuilder{
         ingestionSubsystem2PM10.addCompositionPorts(mrPM10Is2);
         CompositionPort mrPM10Is3 = new CompositionPort(mapReduceProcessorPM10, ingestionSubsystem3PM10);
         ingestionSubsystem3PM10.addCompositionPorts(mrPM10Is3);
-        CompositionPort mrOIso = new CompositionPort(mapReduceProcessorOzone, ingestionSubsystemOzone);
-        ingestionSubsystemOzone.addCompositionPorts(mrOIso);
-
-        CompositionPort iotBIso = new CompositionPort(iotBroker, ingestionSubsystemOzone);
-        ingestionSubsystemOzone.addCompositionPorts(iotBIso);
 
         CompositionPort is1Ps = new CompositionPort(ingestionSubsystem1PM10, persistenceSystem);
         CompositionPort is2Ps = new CompositionPort(ingestionSubsystem2PM10, persistenceSystem);
         CompositionPort is3Ps = new CompositionPort(ingestionSubsystem3PM10, persistenceSystem);
-        CompositionPort isoPs = new CompositionPort(ingestionSubsystemOzone, persistenceSystem);
-        persistenceSystem.addCompositionPorts(is1Ps, is2Ps, is3Ps, isoPs);
+        persistenceSystem.addCompositionPorts(is1Ps, is2Ps, is3Ps);
 
         CompositionPort dsPs = new CompositionPort(dataStorage, persistenceSystem);
         persistenceSystem.addCompositionPorts(dsPs);
@@ -100,8 +83,6 @@ public class PollutionMonitorIdealDesignBuilder{
         // Definizione di Fault Mode Endogeni
 
 
-        EndogenousFaultMode enFM_dosi = new EndogenousFaultMode("DefectiveOzoneSensorIssue");
-        enFM_dosi.setArisingPDF("exp(1/365)");
         EndogenousFaultMode enFM_dpm10s1 = new EndogenousFaultMode("DefectivePM10Sensor1Issue");
         enFM_dpm10s1.setArisingPDF("exp(1/365)");
         EndogenousFaultMode enFM_dpm10s2 = new EndogenousFaultMode("DefectivePM10Sensor2Issue");
@@ -109,16 +90,12 @@ public class PollutionMonitorIdealDesignBuilder{
         EndogenousFaultMode enFM_dpm10s3 = new EndogenousFaultMode("DefectivePM10Sensor3Issue");
         enFM_dpm10s3.setArisingPDF("exp(1/365)");
 
-        faultModes.put(enFM_dosi.getName(), enFM_dosi);
         faultModes.put(enFM_dpm10s1.getName(), enFM_dpm10s1);
         faultModes.put(enFM_dpm10s2.getName(), enFM_dpm10s2);
         faultModes.put(enFM_dpm10s3.getName(), enFM_dpm10s3);
 
         // Definizione di Fault Mode Esogeni
 
-        ExogenousFaultMode exFM_odsensf = new ExogenousFaultMode("OzoneDataSensorFault");
-        ExogenousFaultMode exFM_odsynf = new ExogenousFaultMode("OzoneDataSynthesisFault");
-        ExogenousFaultMode exFM_odrf = new ExogenousFaultMode("OzoneDataRefinementFault");
         ExogenousFaultMode exFM_pm10ds1f = new ExogenousFaultMode("PM10DataSensor1Fault");
         ExogenousFaultMode exFM_pm10ds2f = new ExogenousFaultMode("PM10DataSensor2Fault");
         ExogenousFaultMode exFM_pm10ds3f = new ExogenousFaultMode("PM10DataSensor3Fault");
@@ -126,9 +103,6 @@ public class PollutionMonitorIdealDesignBuilder{
         ExogenousFaultMode exFM_pm10drf = new ExogenousFaultMode("PM10DataRefinementFault");
         ExogenousFaultMode exFM_acdf = new ExogenousFaultMode("AnalysisCorruptedDataFault");
 
-        faultModes.put(exFM_odsensf.getName(), exFM_odsensf);
-        faultModes.put(exFM_odsynf.getName(), exFM_odsynf);
-        faultModes.put(exFM_odrf.getName(), exFM_odrf);
         faultModes.put(exFM_pm10ds1f.getName(), exFM_pm10ds1f);
         faultModes.put(exFM_pm10ds2f.getName(), exFM_pm10ds2f);
         faultModes.put(exFM_pm10ds3f.getName(), exFM_pm10ds3f);
@@ -179,42 +153,6 @@ public class PollutionMonitorIdealDesignBuilder{
         ingestionSubsystem3PM10.addPropagationPort(new PropagationPort(fM_pm10ds3f, exFM_pm10ds3f, mapReduceProcessorPM10));
 
 
-        FailureMode fM_ozonedsensf = new FailureMode("OzoneDataSensorFailure");
-        ErrorMode eM_ISO = new ErrorMode("ingestionSubsystem_prop");
-        eM_ISO.addInputFaultMode(enFM_dosi);
-        eM_ISO.addOutputFailureMode(fM_ozonedsensf);
-        eM_ISO.setEnablingCondition("DefectiveOzoneSensorIssue", faultModes);
-        eM_ISO.setPDF("exp(48)");
-        errorModes.put(eM_ISO.getName(), eM_ISO);
-        failureModes.put(fM_pm10ds3f.getDescription(), fM_pm10ds3f);
-
-        ingestionSubsystemOzone.addErrorMode(eM_ISO);
-
-        ingestionSubsystemOzone.addPropagationPort(new PropagationPort(fM_ozonedsensf, exFM_odsensf, mapReduceProcessorOzone));
-
-        FailureMode fM_ozonedsynf = new FailureMode("OzoneDataSynthesisFailure");
-        ErrorMode em_odsp = new ErrorMode("OzoneDataSynthesis_prop");
-        em_odsp.addInputFaultMode(exFM_odsensf);
-        em_odsp.addOutputFailureMode(fM_ozonedsynf);
-        em_odsp.setEnablingCondition("1/1(OzoneDataSensorFault)", faultModes);
-        em_odsp.setPDF("dirac(0)");
-        errorModes.put(em_odsp.getName(), em_odsp);
-        failureModes.put(fM_ozonedsynf.getDescription(), fM_ozonedsynf);
-
-        FailureMode fM_ozonedrf = new FailureMode("OzoneDataRefinementFailure");
-        ErrorMode em_mrop = new ErrorMode("mapreduceprocessorOzone_prop");
-        em_mrop.addInputFaultMode(exFM_odsynf);
-        em_mrop.addOutputFailureMode(fM_ozonedrf);
-        em_mrop.setEnablingCondition("OzoneDataSynthesisFault", faultModes);
-        em_mrop.setPDF("erlang(5,2)");
-        errorModes.put(em_mrop.getName(), em_mrop);
-        failureModes.put(fM_ozonedrf.getDescription(), fM_ozonedrf);
-
-        mapReduceProcessorOzone.addErrorMode(em_odsp, em_mrop);
-        mapReduceProcessorOzone.addPropagationPort(
-                new PropagationPort(fM_ozonedsynf, exFM_odsynf, mapReduceProcessorOzone),
-                new PropagationPort(fM_ozonedrf, exFM_odrf, smartAgent));
-
         FailureMode fM_pm10dsf = new FailureMode("PM10DataSynthesisFailure");
         ErrorMode em_pm10dsfp = new ErrorMode("PM10DataSynthesisFailure_prop_id");
         em_pm10dsfp.addInputFaultMode(exFM_pm10ds1f, exFM_pm10ds2f, exFM_pm10ds3f);
@@ -229,7 +167,7 @@ public class PollutionMonitorIdealDesignBuilder{
         em_mrppm10p.addInputFaultMode(exFM_pm10dsf);
         em_mrppm10p.addOutputFailureMode(fM_pm10drf);
         em_mrppm10p.setEnablingCondition("PM10DataSynthesisFault", faultModes);
-        em_mrppm10p.setPDF("erlang(5,2)");
+        em_mrppm10p.setPDF("erlang(2,2)");
         errorModes.put(em_mrppm10p.getName(), em_mrppm10p);
         failureModes.put(fM_pm10drf.getDescription(), fM_pm10drf);
 
@@ -242,9 +180,9 @@ public class PollutionMonitorIdealDesignBuilder{
 
         FailureMode fM_acdf = new FailureMode("AnalysisCorruptedDataFailure");
         ErrorMode em_cdp = new ErrorMode("corruptedData_prop");
-        em_cdp.addInputFaultMode(exFM_pm10drf, exFM_odrf);
+        em_cdp.addInputFaultMode(exFM_pm10drf);
         em_cdp.addOutputFailureMode(fM_acdf);
-        em_cdp.setEnablingCondition("PM10DataRefinementFault || OzoneDataRefinementFault", faultModes);
+        em_cdp.setEnablingCondition("1/1(PM10DataRefinementFault)", faultModes);
         em_cdp.setPDF("dirac(0)");
         errorModes.put(em_cdp.getName(), em_cdp);
         failureModes.put(fM_acdf.getDescription(), fM_acdf);
--- src/main/java/it/unifi/stlab/launcher/systembuilder/PollutionMonitorPreliminaryDesignBuiler.java
+++ src/main/java/it/unifi/stlab/launcher/systembuilder/PollutionMonitorPreliminaryDesignBuiler.java
@@ -27,18 +27,13 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         Component persistenceSystem = new Component("PersistenceSystem");
         Component smartAgent = new Component("SmartAgent");
         Component dataStorage = new Component("DataStorage");
-        Component ingestionSubsystemOzone = new Component("IngestionSubsystemOzone");
         Component ingestionSubsystem1PM10 = new Component("IngestionSubsystem1PM10");
         Component ingestionSubsystem2PM10 = new Component("IngestionSubsystem2PM10");
         Component ingestionSubsystem3PM10 = new Component("IngestionSubsystem3PM10");
-        Component iotBroker = new Component("IoTBroker");
-        Component fieldDeviceSubsystemOzone = new Component("FieldDeviceSubsystemOzone");
         Component fieldDeviceSubsystem1PM10 = new Component("FieldDeviceSubsystem1PM10");
         Component fieldDeviceSubsystem2PM10 = new Component("FieldDeviceSubsystem2PM10");
         Component fieldDeviceSubsystem3PM10 = new Component("FieldDeviceSubsystem3PM10");
-        Component mapReduceProcessorOzone = new Component("MapReduceProcessorOzone");
         Component mapReduceProcessorPM10 = new Component("MapReduceProcessorPM10");
-        Component ozoneSensor = new Component("OzoneSensor");
         Component pm10Sensor1 = new Component("PM10Sensor1");
         Component pm10Sensor2 = new Component("PM10Sensor2");
         Component pm10Sensor3 = new Component("PM10Sensor3");
@@ -47,10 +42,9 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         system.addComponent(pollutionMonitor,
                 persistenceSystem, smartAgent,
                 dataStorage, ingestionSubsystem1PM10, ingestionSubsystem2PM10, ingestionSubsystem3PM10,
-                ingestionSubsystemOzone, iotBroker,
-                fieldDeviceSubsystemOzone, fieldDeviceSubsystem1PM10, fieldDeviceSubsystem2PM10, fieldDeviceSubsystem3PM10,
-                mapReduceProcessorOzone, mapReduceProcessorPM10,
-                ozoneSensor, pm10Sensor1, pm10Sensor2, pm10Sensor3);
+                fieldDeviceSubsystem1PM10, fieldDeviceSubsystem2PM10, fieldDeviceSubsystem3PM10,
+                mapReduceProcessorPM10,
+                pm10Sensor1, pm10Sensor2, pm10Sensor3);
         system.setTopLevelComponent(pollutionMonitor);
 
         CompositionPort pm10Fds3 = new CompositionPort(pm10Sensor3, fieldDeviceSubsystem3PM10);
@@ -60,17 +54,12 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         CompositionPort pm10Fds1 = new CompositionPort(pm10Sensor1, fieldDeviceSubsystem1PM10);
         fieldDeviceSubsystem1PM10.addCompositionPorts(pm10Fds1);
 
-        CompositionPort osFdsO = new CompositionPort(ozoneSensor, fieldDeviceSubsystemOzone);
-        fieldDeviceSubsystemOzone.addCompositionPorts(osFdsO);
-
         CompositionPort fds1Is = new CompositionPort(fieldDeviceSubsystem1PM10, ingestionSubsystem1PM10);
         ingestionSubsystem1PM10.addCompositionPorts(fds1Is);
         CompositionPort fds2Is = new CompositionPort(fieldDeviceSubsystem2PM10, ingestionSubsystem2PM10);
         ingestionSubsystem2PM10.addCompositionPorts(fds2Is);
         CompositionPort fds3Is = new CompositionPort(fieldDeviceSubsystem3PM10, ingestionSubsystem3PM10);
         ingestionSubsystem3PM10.addCompositionPorts(fds3Is);
-        CompositionPort fdsoIs = new CompositionPort(fieldDeviceSubsystemOzone, ingestionSubsystemOzone);
-        ingestionSubsystemOzone.addCompositionPorts(fdsoIs);
 
         CompositionPort mrPM10Is1 = new CompositionPort(mapReduceProcessorPM10, ingestionSubsystem1PM10);
         ingestionSubsystem1PM10.addCompositionPorts(mrPM10Is1);
@@ -78,17 +67,11 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         ingestionSubsystem2PM10.addCompositionPorts(mrPM10Is2);
         CompositionPort mrPM10Is3 = new CompositionPort(mapReduceProcessorPM10, ingestionSubsystem3PM10);
         ingestionSubsystem3PM10.addCompositionPorts(mrPM10Is3);
-        CompositionPort mrOIso = new CompositionPort(mapReduceProcessorOzone, ingestionSubsystemOzone);
-        ingestionSubsystemOzone.addCompositionPorts(mrOIso);
-
-        CompositionPort iotBIso = new CompositionPort(iotBroker, ingestionSubsystemOzone);
-        ingestionSubsystemOzone.addCompositionPorts(iotBIso);
 
         CompositionPort is1Ps = new CompositionPort(ingestionSubsystem1PM10, persistenceSystem);
         CompositionPort is2Ps = new CompositionPort(ingestionSubsystem2PM10, persistenceSystem);
         CompositionPort is3Ps = new CompositionPort(ingestionSubsystem3PM10, persistenceSystem);
-        CompositionPort isoPs = new CompositionPort(ingestionSubsystemOzone, persistenceSystem);
-        persistenceSystem.addCompositionPorts(is1Ps, is2Ps, is3Ps, isoPs);
+        persistenceSystem.addCompositionPorts(is1Ps, is2Ps, is3Ps);
 
         CompositionPort dsPs = new CompositionPort(dataStorage, persistenceSystem);
         persistenceSystem.addCompositionPorts(dsPs);
@@ -100,14 +83,6 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         // Definizione di Fault Mode Endogeni
 
 
-        EndogenousFaultMode enFM_dosi = new EndogenousFaultMode("DefectiveOzoneSensorIssue");
-        enFM_dosi.setArisingPDF("exp(1/365)");
-        EndogenousFaultMode enFM_poti = new EndogenousFaultMode("PublisherOzoneTopicIssue");
-        enFM_poti.setArisingPDF("exp(1/60)");
-        EndogenousFaultMode enFM_soti = new EndogenousFaultMode("SubscriberOzoneTopicIssue");
-        enFM_soti.setArisingPDF("exp(1/60)");
-        EndogenousFaultMode enFM_oswmrbi = new EndogenousFaultMode("OzoneSWMRBugIssue");
-        enFM_oswmrbi.setArisingPDF("exp(1/180)");
         EndogenousFaultMode enFM_dpm10s1 = new EndogenousFaultMode("DefectivePM10Sensor1Issue");
         enFM_dpm10s1.setArisingPDF("exp(1/365)");
         EndogenousFaultMode enFM_dpm10s2 = new EndogenousFaultMode("DefectivePM10Sensor2Issue");
@@ -125,12 +100,8 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         EndogenousFaultMode enFM_pm10swmrbi = new EndogenousFaultMode("PM10SWMRBugIssue");
         enFM_pm10swmrbi.setArisingPDF("exp(1/180)");
         EndogenousFaultMode enFM_swb= new EndogenousFaultMode("SmartAgent_SWBug");
-        enFM_swb.setArisingPDF("exp(1/360)");
+        enFM_swb.setArisingPDF("exp(1/730)");
 
-        faultModes.put(enFM_dosi.getName(), enFM_dosi);
-        faultModes.put(enFM_poti.getName(), enFM_poti);
-        faultModes.put(enFM_soti.getName(), enFM_soti);
-        faultModes.put(enFM_oswmrbi.getName(), enFM_oswmrbi);
         faultModes.put(enFM_dpm10s1.getName(), enFM_dpm10s1);
         faultModes.put(enFM_dpm10s2.getName(), enFM_dpm10s2);
         faultModes.put(enFM_dpm10s3.getName(), enFM_dpm10s3);
@@ -143,9 +114,6 @@ public class PollutionMonitorPreliminaryDesignBuiler {
 
         // Definizione di Fault Mode Esogeni
 
-        ExogenousFaultMode exFM_odsensf = new ExogenousFaultMode("OzoneDataSensorFault");
-        ExogenousFaultMode exFM_odsynf = new ExogenousFaultMode("OzoneDataSynthesisFault");
-        ExogenousFaultMode exFM_odrf = new ExogenousFaultMode("OzoneDataRefinementFault");
         ExogenousFaultMode exFM_pm10ds1f = new ExogenousFaultMode("PM10DataSensor1Fault");
         ExogenousFaultMode exFM_pm10ds2f = new ExogenousFaultMode("PM10DataSensor2Fault");
         ExogenousFaultMode exFM_pm10ds3f = new ExogenousFaultMode("PM10DataSensor3Fault");
@@ -153,9 +121,6 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         ExogenousFaultMode exFM_pm10drf = new ExogenousFaultMode("PM10DataRefinementFault");
         ExogenousFaultMode exFM_acdf = new ExogenousFaultMode("AnalysisCorruptedDataFault");
 
-        faultModes.put(exFM_odsensf.getName(), exFM_odsensf);
-        faultModes.put(exFM_odsynf.getName(), exFM_odsynf);
-        faultModes.put(exFM_odrf.getName(), exFM_odrf);
         faultModes.put(exFM_pm10ds1f.getName(), exFM_pm10ds1f);
         faultModes.put(exFM_pm10ds2f.getName(), exFM_pm10ds2f);
         faultModes.put(exFM_pm10ds3f.getName(), exFM_pm10ds3f);
@@ -206,42 +171,6 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         ingestionSubsystem3PM10.addPropagationPort(new PropagationPort(fM_pm10ds3f, exFM_pm10ds3f, mapReduceProcessorPM10));
 
 
-        FailureMode fM_ozonedsensf = new FailureMode("OzoneDataSensorFailure");
-        ErrorMode eM_ISO = new ErrorMode("ingestionSubsystem_prop");
-        eM_ISO.addInputFaultMode(enFM_dosi, enFM_poti);
-        eM_ISO.addOutputFailureMode(fM_ozonedsensf);
-        eM_ISO.setEnablingCondition("DefectiveOzoneSensorIssue || PublisherOzoneTopicIssue", faultModes);
-        eM_ISO.setPDF("exp(48)");
-        errorModes.put(eM_ISO.getName(), eM_ISO);
-        failureModes.put(fM_pm10ds3f.getDescription(), fM_pm10ds3f);
-
-        ingestionSubsystemOzone.addErrorMode(eM_ISO);
-
-        ingestionSubsystemOzone.addPropagationPort(new PropagationPort(fM_ozonedsensf, exFM_odsensf, mapReduceProcessorOzone));
-
-        FailureMode fM_ozonedsynf = new FailureMode("OzoneDataSynthesisFailure");
-        ErrorMode em_odsp = new ErrorMode("OzoneDataSynthesis_prop");
-        em_odsp.addInputFaultMode(exFM_odsensf);
-        em_odsp.addOutputFailureMode(fM_ozonedsynf);
-        em_odsp.setEnablingCondition("1/1(OzoneDataSensorFault)", faultModes);
-        em_odsp.setPDF("dirac(0)");
-        errorModes.put(em_odsp.getName(), em_odsp);
-        failureModes.put(fM_ozonedsynf.getDescription(), fM_ozonedsynf);
-
-        FailureMode fM_ozonedrf = new FailureMode("OzoneDataRefinementFailure");
-        ErrorMode em_mrop = new ErrorMode("mapreduceprocessorOzone_prop");
-        em_mrop.addInputFaultMode(exFM_odsynf, enFM_soti, enFM_oswmrbi);
-        em_mrop.addOutputFailureMode(fM_ozonedrf);
-        em_mrop.setEnablingCondition("OzoneDataSynthesisFault || SubscriberOzoneTopicIssue || OzoneSWMRBugIssue", faultModes);
-        em_mrop.setPDF("erlang(5,2)");
-        errorModes.put(em_mrop.getName(), em_mrop);
-        failureModes.put(fM_ozonedrf.getDescription(), fM_ozonedrf);
-
-        mapReduceProcessorOzone.addErrorMode(em_odsp, em_mrop);
-        mapReduceProcessorOzone.addPropagationPort(
-                new PropagationPort(fM_ozonedsynf, exFM_odsynf, mapReduceProcessorOzone),
-                new PropagationPort(fM_ozonedrf, exFM_odrf, smartAgent));
-
         FailureMode fM_pm10dsf = new FailureMode("PM10DataSynthesisFailure");
         ErrorMode em_pm10dsfp = new ErrorMode("PM10DataSynthesisFailure_prop_id");
         em_pm10dsfp.addInputFaultMode(exFM_pm10ds1f, exFM_pm10ds2f, exFM_pm10ds3f);
@@ -256,7 +185,7 @@ public class PollutionMonitorPreliminaryDesignBuiler {
         em_mrppm10p.addInputFaultMode(exFM_pm10dsf, enFM_spm10ti, enFM_pm10swmrbi);
         em_mrppm10p.addOutputFailureMode(fM_pm10drf);
         em_mrppm10p.setEnablingCondition("PM10DataSynthesisFault || SubscriberPM10TopicIssue || PM10SWMRBugIssue", faultModes);
-        em_mrppm10p.setPDF("erlang(5,2)");
+        em_mrppm10p.setPDF("erlang(2,2)");
         errorModes.put(em_mrppm10p.getName(), em_mrppm10p);
         failureModes.put(fM_pm10drf.getDescription(), fM_pm10drf);
 
@@ -269,9 +198,9 @@ public class PollutionMonitorPreliminaryDesignBuiler {
 
         FailureMode fM_acdf = new FailureMode("AnalysisCorruptedDataFailure");
         ErrorMode em_cdp = new ErrorMode("corruptedData_prop");
-        em_cdp.addInputFaultMode(exFM_pm10drf, exFM_odrf);
+        em_cdp.addInputFaultMode(exFM_pm10drf);
         em_cdp.addOutputFailureMode(fM_acdf);
-        em_cdp.setEnablingCondition("PM10DataRefinementFault || OzoneDataRefinementFault", faultModes);
+        em_cdp.setEnablingCondition("1/1(PM10DataRefinementFault)", faultModes);
         em_cdp.setPDF("dirac(0)");
         errorModes.put(em_cdp.getName(), em_cdp);
         failureModes.put(fM_acdf.getDescription(), fM_acdf);
